e9e2f2e3fb834ce1cccbf3b9c3a76f6ecab86c68
GenkiSugiyama <sgym0318marcos@gmail.com>
2020-06-30 21:27:21 +0900

    論理型について

---
+++ base3.py
@@ -0,0 +1,18 @@
+# 論理型：TrueとFalseの2つの変数を扱う型
+is_animal = True
+if is_animal:
+  print('動物です')
+
+is_man = True
+is_adult = False
+
+# or文
+if is_man or is_adult:
+  print('男か大人です')
+else:
+  print('男でも大人でもありません')
+
+# and文
+if is_man and is_adult:
+  print('成人男性です')
+
