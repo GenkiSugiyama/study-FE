571f8474afe857cc26c19e18861ba90ebea308ef
GenkiSugiyama <sgym0318marcos@gmail.com>
2020-06-30 21:06:08 +0900

    udemyのFE試験勉強（python使用）

---
+++ sample.py
@@ -0,0 +1,16 @@
+# 標準出力 print()
+# print('Hello Worldn')
+
+# 標準入力input()
+# name = input('あなたの名前はなんですか : ')
+# print(name)
+
+# format関数
+# '任意の文字列{}任意の文字列'.format(変数) で文字列に埋め込まれたの{}内に変数を展開する
+# name = input('あなたの名前は何ですか？：')
+# age = input('年齢は？：')
+# print('私の名前は{}です、年齢は{}です'.format(name, age))
+
+a = b = 'hello'
+
+print(a, b)
