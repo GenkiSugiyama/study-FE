9a76c1c5313081aed5ddd651304249d4a58c556a
GenkiSugiyama <sgym0318marcos@gmail.com>
2020-06-30 21:48:51 +0900

    数値について

---
+++ base4.py
@@ -0,0 +1,29 @@
+# 数値型
+value = 1
+# print(value)
+# value = -5
+# print(value)
+
+# value += 4
+# print(value) # -1
+
+# value *= -8
+# print(value)
+# print(value / 3) # 小数点以下も計算
+# print(value // 3) # 小数点以下切り下げ
+# print(value % 3)
+
+
+value += 3
+print(value)
+# べき乗
+print(value ** 2)
+
+# 浮動小数点数
+height = 175.5
+print(type(height)) # float
+
+# シフト演算
+value = 8
+print(value >> 2) # 8は二進数で1000　これを右に2シフトすると0010 0010は10進数で2
+print(value << 2) #8を左に2シフトすると100000→32
